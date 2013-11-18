7c4efa1145fa8d8226645efa1b53851755dc3ba0
Glen Barber <51638+glenjbarber@users.noreply.github.com>
2013-11-18 18:08:25 +0000

    Add quick and dirty script to get build checksums and print to stdout.
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: file:///Users/gjb/Downloads/releng-svn/thermite-r688@49 5711f056-dcf5-4cc1-a278-612e92bcda7e

---
+++ user/gjb/scripts/release/get-checksums.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+#
+# $relengid$
+#
+
+quick_usage() {
+	echo "$(basename ${0}) /path/to/configuration/file"
+	exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+	quick_usage
+fi
+
+. $(dirname $(basename ${0}))/${1}
+
+readfiles() {
+}
+
+get_vm_checksum() {
+	local _s="${r}-${a}-${t}"
+	sumfiles="SHA256 MD5"
+	if [ -e ${scriptdir}/${_s}.conf ]; then
+		. ${scriptdir}/${_s}.conf
+	else
+		return 0
+	fi
+	if [ ! -e ${CHROOTDIR}/vmimage/ ]; then
+		return 0
+	fi
+	__REVISION=$(make -C ${CHROOTDIR}/usr/src/release -V REVISION)
+	__BRANCH=$(make -C ${CHROOTDIR}/usr/src/release -V BRANCH)
+	for _f in ${sumfiles}; do
+		case ${_f} in
+			SHA256)
+				echo "- ${__REVISION}-${__BRANCH} ${a}:"
+				;;
+			*)
+				;;
+		esac
+		cat ${CHROOTDIR}/vmimage/CHECKSUM.${_f}* | \
+			sed -e 's/^/        /'
+		echo
+	done
+	echo
+	return 0
+}
+
+
+get_iso_checksum() {
+	local _s="${r}-${a}-${t}"
+	sumfiles="SHA256 MD5"
+	if [ -e ${scriptdir}/${_s}.conf ]; then
+		. ${scriptdir}/${_s}.conf
+	else
+		return 0
+	fi
+	if [ ! -e ${CHROOTDIR}/R/ ]; then
+		return 0
+	fi
+	__REVISION=$(make -C ${CHROOTDIR}/usr/src/release -V REVISION)
+	__BRANCH=$(make -C ${CHROOTDIR}/usr/src/release -V BRANCH)
+	for _f in ${sumfiles}; do
+		case ${_f} in
+			SHA256)
+				echo "- ${__REVISION}-${__BRANCH} ${a}:"
+				;;
+			*)
+				;;
+		esac
+		cat ${CHROOTDIR}/R/CHECKSUM.${_f}* | \
+			sed -e 's/^/        /'
+		echo
+	done
+	echo
+	return 0
+}
+
+main() {
+	echo "== ISO CHECKSUMS =="
+	echo
+	for r in ${revs}; do
+		for a in ${archs}; do
+			for t in ${types}; do
+				get_iso_checksum
+			done
+		done
+	done
+	echo "== VM IMAGE CHECKSUMS =="
+	echo
+	for r in ${revs}; do
+		for a in ${archs}; do
+			for t in ${types}; do
+				case ${a} in
+					amd64|i386)
+						get_vm_checksum
+						;;
+					*)
+					;;
+				esac
+			done
+		done
+	done
+}
+
+main
+
