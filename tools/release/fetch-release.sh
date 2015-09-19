b552b4000d12676233b3176da23db203c4332d4a
Glen Barber <51638+glenjbarber@users.noreply.github.com>
2015-09-19 01:10:20 +0000

    Remove ineffective trap.
    Fix mailing to send from generic, non-human account to avoid
    confusion.
    
    While here, chmod 775 files in the snapshots/ directory, though
    I think this is not needed anymore.  Peter noted it was necessary
    back before I was even on re@, so followup is needed.
    
    Note that snap and ftp-master do not automatically sync RELEASE
    builds in email output.  Once ftp-master moves and snap is no
    longer needed for the temporary staging area, marius@ will be
    given access to ftp-master as part of his new role.
    
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: file:///Users/gjb/Downloads/releng-svn/thermite-r688@412 5711f056-dcf5-4cc1-a278-612e92bcda7e

--- tools/release/fetch-release.sh
+++ tools/release/fetch-release.sh
@@ -3,9 +3,16 @@
 # $Id$
 #
 
-trap 'exit' 1 2 3 4 5 6 7 8 9 10 11
+emailgoesto="gjb@FreeBSD.org marius@FreeBSD.org"
+emailsentfrom="root@FreeBSD.org"
+
+_subject="release FTP sync done"
+_body="Sync done.\n\n"
+_body="${_body}RELEASE builds do not automatically sync to ftp-master.\n\n"
+_body="${_body}Contact clusteradm@ for now."
+
 while ! rsync --progress --partial --time-limit=5 -avH --delete rsync://releng1.nyi.freebsd.org/releases /snap/stage/releases; do :; done
 find /snap/stage/releases/ -type d | xargs chmod 775
 
-#echo "sync done" | mail -s "RELEASE FTP sync done" gjb@FreeBSD.org
-
+printf "From: ${emailsentfrom}\nTo: ${emailgoesto}\nSubject: ${_subject}\n\n${_body}\n\n" \
+	| /usr/sbin/sendmail -oi -f ${emailsentfrom} ${emailgoesto}
--- tools/release/fetch-snap.sh
+++ tools/release/fetch-snap.sh
@@ -3,14 +3,19 @@
 # $Id$
 #
 
-trap 'exit' 1 2 3 4 5 6 7 8 9 10 11
+emailgoesto="gjb@FreeBSD.org marius@FreeBSD.org"
+emailsentfrom="root@FreeBSD.org"
+
+_subject="snapshot FTP sync done"
+_body="Sync done."
 
 # 11-CURRENT
 while ! rsync --progress --partial --time-limit=5 -avH rsync://releng2.nyi.freebsd.org/snapshots /snap/stage/snapshots; do :; done
 # 10-STABLE
 while ! rsync --progress --partial --time-limit=5 -avH rsync://releng1.nyi.freebsd.org/snapshots /snap/stage/snapshots; do :; done
 
+find /snap/stage/snapshots/ -type d | xargs chmod 775
 rsync -avH /snap/stage/snapshots/* /snap/ftp/snapshots/
 
-echo "Sync done" | mail -s "snap FTP sync done" gjb@FreeBSD.org
-
+printf "From: ${emailsentfrom}\nTo: ${emailgoesto}\nSubject: ${_subject}\n\n${_body}\n\n" \
+	| /usr/sbin/sendmail -oi -f ${emailsentfrom} ${emailgoesto}
