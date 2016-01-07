8a711f0a33e1d5cb3bd68691a22bdc387409685d
Glen Barber <51638+glenjbarber@users.noreply.github.com>
2016-01-07 00:15:20 +0000

    Reverse the order of snapshot rsync to fetch 10-STABLE first. Now that xz(1) in 10-STABLE supports multiple threads, 10.x builds complete much faster than 11.x again.  Horray for that.
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: file:///Users/gjb/Downloads/releng-svn/thermite-r688@449 5711f056-dcf5-4cc1-a278-612e92bcda7e

--- tools/release/fetch-snap.sh
+++ tools/release/fetch-snap.sh
@@ -9,10 +9,10 @@ emailsentfrom="root@FreeBSD.org"
 _subject="snapshot FTP sync done"
 _body="Sync done.\n\n"
 
-# 11-CURRENT
-while ! rsync --progress --partial --time-limit=5 -avH rsync://releng2.nyi.freebsd.org/snapshots /snap/stage/snapshots; do :; done
 # 10-STABLE
 while ! rsync --progress --partial --time-limit=5 -avH rsync://releng1.nyi.freebsd.org/snapshots /snap/stage/snapshots; do :; done
+# 11-CURRENT
+while ! rsync --progress --partial --time-limit=5 -avH rsync://releng2.nyi.freebsd.org/snapshots /snap/stage/snapshots; do :; done
 
 find /snap/stage/snapshots/ -type d | xargs chmod 775
 rsync -avH /snap/stage/snapshots/* /snap/ftp/snapshots/
