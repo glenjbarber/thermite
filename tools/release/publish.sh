438c9728faae668b301204d61d9cc5c12add06eb
Glen Barber <51638+glenjbarber@users.noreply.github.com>
2017-03-23 17:53:24 +0000

    Remove deprecated fetch-release.sh and fetch-snap.sh scripts. Add the new fetch-snapshots.sh script. Add the publish.sh script used on ftp-master.
    Sponsored by:	The FreeBSD Foundation
    
    
    git-svn-id: file:///Users/gjb/Downloads/releng-svn/thermite-r688@493 5711f056-dcf5-4cc1-a278-612e92bcda7e

--- tools/release/publish.sh
+++ tools/release/publish.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
-# $Id$
+#
+# $FreeBSD$
+#
 
 hn=$(hostname | tr A-Z a-z)
+me=$(id -un)
 
 if [ "${hn}" != "ftp-master.freebsd.org" ]; then
+	echo "For use on ftp-master only."
 	exit 1
 fi
-cd /archive/tmp/releases
-pax -r -w -l . /archive/pub/FreeBSD/releases
+
+if [ "x${me}" != "xarchive" ]; then
+	echo "Must be run by the 'archive' user."
+	exit 1
+fi
+
+#cd /archive/tmp/releases
+#pax -r -w -l . /archive/pub/FreeBSD/releases
+#/usr/local/bin/rsync -avH /archive/tmp/releases/* /archive/pub/FreeBSD/releasees/
+
+cd /archive/tmp/snapshots
+pax -r -w -l . /archive/pub/FreeBSD/snapshots
+/usr/local/bin/rsync -avH /archive/tmp/snapshots/* /archive/pub/FreeBSD/snapshots/
