ad4b5a384d162a94361d58ff164e214966eb8542
Glen Barber <51638+glenjbarber@users.noreply.github.com>
2020-09-03 00:59:21 +0000

    Revert r2297
    Sponsored by:	Rubicon Communications, LLC (netgate.com)
    
    
    git-svn-id: file:///Users/gjb/Downloads/releng-svn/thermite-r688@608 5711f056-dcf5-4cc1-a278-612e92bcda7e

--- tools/git-release/thermite.sh
+++ tools/git-release/thermite.sh
@@ -210,7 +210,7 @@ zfs_create_tree() {
 	info "Creating ${_clone}"
 	zfs create -o atime=off -o mountpoint=${_mount} ${_clone}
 	info "Source checkout ${_gitsrc} to ${_mount}"
-	git -C ${_mount} clone -q -b ${TREEBRANCH} ${_gitsrc}
+	git clone -q -b ${TREEBRANCH} ${_gitsrc} ${_mount}
 	info "Creating ZFS snapshot ${_clone}@clone"
 	zfs snapshot ${_clone}@clone
 	eval zfs_${_tree}_seed_${rev}_${type}=1
