e083ea7a138a53bca2f127c407aa19ed8a7d45e4
Glen Barber <51638+glenjbarber@users.noreply.github.com>
2020-12-23 15:23:23 +0000

    Add a script to output what ZFS datasets would be created based on the list of various combinations used by thermite.sh.
    This is particularly useful for verifying a new architecture/kernel
    pair will be created when a change is made to either the configuration
    itself, or ${SRCTOP}/release/${TARGET_ARCH}/${KERNEL}.conf.
    
    Sponsored by:	Rubicon Communications, LLC (netgate.com)
    
    
    git-svn-id: file:///Users/gjb/Downloads/releng-svn/thermite-r688@629 5711f056-dcf5-4cc1-a278-612e92bcda7e

---
+++ tools/git-release/fstest.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+runcmd() {
+	eval "$@"
+}
+
+loop_revs() {
+	for rev in ${revs}; do
+	        eval runcmd "$@"
+	done
+	unset rev
+}
+
+loop_archs() {
+	for arch in ${archs}; do
+	        eval runcmd "$@"
+	done
+	unset arch
+}
+
+loop_kernels() {
+	for kernel in ${kernels}; do
+	        eval runcmd "$@"
+	done
+	unset kernel
+}
+
+loop_types() {
+	for type in ${types}; do
+	        eval runcmd "$@"
+	done
+	unset type
+}
+
+runall() {
+	eval loop_revs loop_archs loop_kernels loop_types "$@"
+}
+
+source_config() {
+	local configfile
+	configfile="${scriptdir}/${rev}-${arch}-${kernel}-${type}.conf"
+	if [ ! -e "${configfile}" ]; then
+	        return 1
+	fi
+	. "${configfile}"
+	return 0
+}
+
+mk_dataset() {
+	source_config || return 0
+	case ${arch} in
+		i386)
+			_chrootarch="i386"
+			;;
+		*)
+			_chrootarch="amd64"
+		;;
+	esac
+	[ ! -z $(eval echo \${zfs_${_chrootarch}_seed_${rev}_${type}}) ] \
+		&& return 0
+	_clone="${zfs_parent}/${rev}-${_chrootarch}-worldseed-${type}"
+	_mount="/${zfs_mount}/${rev}-${arch}-worldseed-${type}"
+	_build="${rev}-${arch}-${kernel}-${type}"
+	_dest="${__WRKDIR_PREFIX}/${_build}"
+	[ "${debug}" ] && \
+		echo "Creating fake ZFS dataset clone \"${_clone}\""
+	echo zfs create -o mountpoint="${_mount}" "${_clone}"
+	echo zfs snapshot ${_clone}@clone
+
+	[ "${debug}" ] && \
+		echo "Cloning ${_chrootarch} world to ${zfs_parent}/${_build}"
+	echo zfs clone -p -o atime=off -o mountpoint=${_dest} \
+		${_clone}@clone ${zfs_parent}/${_build}
+	unset _clone _mount _build _dest
+	
+	return 0
+}
+
+usage() {
+	echo "$(realpath $(basename ${0})) -c config"
+	echo "$(realpath $(basename ${0})) -c config -d"
+	echo "    (debug mode)"
+	exit 1
+}
+
+main() {
+	export __BUILDCONFDIR="$(dirname $(realpath ${0}))"
+	while getopts "c:d" opt; do
+		case ${opt} in
+			c)
+				CONF="${OPTARG}"
+				[ ! -e ${CONF} ] && usage
+				. $(realpath ${CONF})
+				;;
+			d)
+				debug=1
+				;;
+			\?)
+				usage
+				;;
+		esac
+	done
+	[ -z "${CONF}" ] && usage
+
+	runall mk_dataset
+
+	return 0
+}
+
+main "${@}"
